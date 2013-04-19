d75fca0cf1b2c1092cfa9180f6ce13030ba4ed8f
Daniel Imrie-Situnayake <dansmailbag@googlemail.com>
2013-04-18 18:50:33 -0700

    Random stars the proper way

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 require(['d3', 'lib/domReady!'], function(d3, doc){
 
   var starfield = d3.select('.starfield');
-  console.log(starfield);
 
-  var stars = generateStars();
+  var WIDTH = d3.select('.starfield')[0][0].clientWidth;
+  var HEIGHT = d3.select('.starfield')[0][0].clientHeight;
+
+  // var stars = generateStars();
+  var stars = {
+    small: d3.range(100).map(function() {
+      return {
+        x: Math.floor(Math.random()*WIDTH+1)
+        , y: Math.floor(Math.random()*WIDTH+1)
+      };
+    })
+  };
 
   var dimStar = function(chain){
     console.log(chain);
     console.log(this);
     chain.append('rect')
       .attr('class', 'star dim')
-      .attr('x', function(data){return data[0]})
-      .attr('y', function(data){return data[1]})
+      .attr('x', function(star){return star.x})
+      .attr('y', function(star){return star.y})
       .attr('width', 4)
       .attr('height', 4);
   };
@@ -19,8 +29,8 @@ require(['d3', 'lib/domReady!'], function(d3, doc){
   var brightStar = function(chain){
     chain.append('rect')
       .attr('class', 'star bright')
-      .attr('x', function(data){return data[0]})
-      .attr('y', function(data){return data[1]})
+      .attr('x', function(star){return star.x})
+      .attr('y', function(star){return star.y})
       .attr('width', 4)
       .attr('height', 4);
   };
@@ -32,31 +42,3 @@ require(['d3', 'lib/domReady!'], function(d3, doc){
     .call(dimStar);
 
 });
-
-function generateStars(){
-
-  var SMALL_COUNT = 100;
-
-  var BIG_COUNT = 10;
-
-  var stars = {
-    small: []
-    , big: []
-  };
-
-  var WIDTH = d3.select('.starfield')[0][0].clientWidth;
-  var HEIGHT = d3.select('.starfield')[0][0].clientHeight;
-
-  for(var i=0; i<=SMALL_COUNT; i++){
-    
-    var x = Math.floor(Math.random()*WIDTH+1)
-    var y = Math.floor(Math.random()*HEIGHT+1)
-
-    console.log(WIDTH);
-    stars.small.push([x,y]);
-  
-  }
-
-  return stars;
-
-}
